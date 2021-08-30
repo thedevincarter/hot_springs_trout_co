da72772e71f6f59433c76fbc6772961557979b05
Devin Carter <>
2021-08-29 21:14:00 -0700

    add chakra provider and theme wrapper to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
-export default MyApp
+
+const theme = extendTheme({
+  color: {},
+});
+
+export default MyApp;
