6aa0f46ac0c83515f45d8655eec4de745a1a7dd5
Devin Carter <>
2021-08-30 23:07:48 -0700

    basic pages

--- pages/about.tsx
+++ pages/about.tsx
@@ -3,20 +3,20 @@ import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
-const Home: NextPage = () => {
+const About: NextPage = () => {
     return (
         <div className={styles.container}>
             <Head>
-                <title>Troutfitters</title>
-                <meta name="description" content="Wyomings's premier fly fishing guides" />
+                <title>About Troutfitters</title>
+                <meta name="description" content="A brief history of Troutfitters" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
             <main className={styles.main}>
-                <h1 className={styles.title}>Troutfitters Home</h1>
+                <h1 className={styles.title}>Troutfitters About</h1>
             </main>
         </div>
     );
 };
 
-export default Home;
+export default About;
--- pages/services.tsx
+++ pages/services.tsx
@@ -3,20 +3,20 @@ import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
-const Home: NextPage = () => {
+const Services: NextPage = () => {
     return (
         <div className={styles.container}>
             <Head>
-                <title>Troutfitters</title>
-                <meta name="description" content="Wyomings's premier fly fishing guides" />
+                <title>Troutfitters Services</title>
+                <meta name="description" content="Services provided by troutfitters" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
             <main className={styles.main}>
-                <h1 className={styles.title}>Troutfitters Home</h1>
+                <h1 className={styles.title}>Troutfitters Services</h1>
             </main>
         </div>
     );
 };
 
-export default Home;
+export default Services;
